93b1e9234e5ea46304e93055e62f0ad51660c5d4
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-03-20 19:40:04 +0000

    fixing folders

---
+++ src/main.java
@@ -0,0 +1,35 @@
+
+import java.io.File;
+//import ilog.concert.IloException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Othmane
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+                                    //    throws IloException
+                                                            {
+        InputData data=new InputData(new File("Instances\\Solomon\\c101.txt"),50/*stops count*/);
+        System.out.println(data);
+        
+        MetaHeuristic algorithm=new GeneticAlgorithm(data);
+        algorithm.Run(5/*run time in second*/);
+
+        if(algorithm.isFeasible()){
+            System.out.println(algorithm.getBestSolution());
+//            algorithm.getBestSolution().DrawGraph(data);
+//            new CplexModel(data,algorithm.getBestSolution().getRoutesCount()).Solve();
+        }
+    }
+}
---
+++ src/main2.java
@@ -0,0 +1,37 @@
+
+import java.io.File;
+import java.util.stream.Stream;
+//import ilog.concert.IloException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Othmane
+ */
+public class main2 {
+    public static void main(String[] args)
+                                    //    throws IloException
+                                                            {
+        Stream.of(new File("instances\\Solomon").listFiles())
+                .map(file->new InputData(file,50/*stops count*/))
+                .forEach(data->{
+                    System.out.println(data);
+
+                    MetaHeuristic algorithm=new GeneticAlgorithm(data);
+                    algorithm.Run(5/*run time in second*/);
+
+                    if(algorithm.isFeasible()){
+                        System.out.println(algorithm.getBestSolution());
+//                        algorithm.getBestSolution().DrawGraph(data);
+//                        new CplexModel(data,algorithm.getBestSolution().getRoutesCount()).Solve();
+                    }
+                    System.out.println();
+                    System.gc();
+                });
+    }
+}
